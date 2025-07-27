3dc4fcbea8f489ec8deb8159d6dd5ac6097402ac
Admir Nisic <ni.admir@gmail.com>
2025-07-27 12:53:26 +0200

    remove app local

--- projects/app/src/app/app.routes.ts
+++ projects/app/src/app/app.routes.ts
@@ -1,17 +1,11 @@
 import { Routes } from "@angular/router";
-import { AppLocal } from "./app-local/app-local";
 import { AppProd } from "./app-prod/app-prod";
-import { App } from "./app";
 
 export const routes: Routes = [
   {
-    path: "app-local",
-    component: AppLocal,
-  },
-  {
-    path: "app-prod",
+    path: "",
     component: AppProd,
   },
 
-  { path: "**", redirectTo: "app-prod" },
+  { path: "**", redirectTo: "" },
 ];
